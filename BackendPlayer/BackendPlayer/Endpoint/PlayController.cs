8fee685a7d14b34ce9c2f5f4edd21635f3d45eb8
Myo Set Paing <138693417+KraftReport@users.noreply.github.com>
2024-11-03 00:36:42 +0630

    player

---
+++ BackendPlayer/BackendPlayer/Endpoint/PlayController.cs
@@ -0,0 +1,41 @@
+﻿using BackendPlayer.BackendPlayer.Implementation;
+using BackendPlayer.BackendPlayer.Interface;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BackendPlayer.BackendPlayer.Endpoint
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlayController : ControllerBase
+    {
+        private IMusicPlayer player;
+
+        public PlayController(IMusicPlayer nAudioService)
+        {
+            player = nAudioService;
+        }
+         
+        [HttpPost]
+        public async Task<IActionResult> PlaySong([FromForm]string filepath)
+        {  
+            player.Load(filepath);
+            player.Play();
+            return Ok("song is playing");
+        }
+
+        [HttpPost("/pause")]
+        public async Task<IActionResult> PauseSong()
+        {
+            player.Pause();
+            return Ok("song is paused");
+        }
+
+        [HttpPost("/stop")]
+        public async Task<IActionResult> StopSong()
+        {
+            player.Stop();
+            return Ok("song is stopped");
+        }
+    }
+}
---
+++ BackendPlayer/BackendPlayer/Interface/IMusicPlayer.cs
@@ -0,0 +1,10 @@
+﻿namespace BackendPlayer.BackendPlayer.Interface
+{
+    public interface IMusicPlayer
+    {
+        public void Play();
+        public void Pause();
+        public void Stop();
+        public void Load(string filepath);
+    }
+}
