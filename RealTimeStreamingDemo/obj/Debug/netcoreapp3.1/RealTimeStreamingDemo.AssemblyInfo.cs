9c85b4e6594eb016a9f43b49f26fb6ccda820a8c
Myo Set Paing <138693417+KraftReport@users.noreply.github.com>
2025-01-12 00:03:04 +0630

    testing

--- RealTimeStreamingDemo/obj/Debug/netcoreapp3.1/RealTimeStreamingDemo.AssemblyInfo.cs
+++ RealTimeStreamingDemo/obj/Debug/netcoreapp3.1/RealTimeStreamingDemo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RealTimeStreamingDemo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+439e7fe8aeb2dac51c3a33ecc4f011e692ef0f05")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4714219c917d63ab75d81802dce34b48f97106f7")]
 [assembly: System.Reflection.AssemblyProductAttribute("RealTimeStreamingDemo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RealTimeStreamingDemo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
