557219b48a104bbf9b12c014c1e81acb941fddc8
Myo Set Paing <138693417+KraftReport@users.noreply.github.com>
2025-01-24 07:33:19 +0630

    leet code

---
+++ LeetcodeDebugger/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+/*int[] nums = { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4 };
+
+   int k = 0;
+    int lastInsertNumber;
+    for (int i = 0; i < nums.Length; i++)
+    {
+        int inserted = 0;
+        do
+        {
+            nums[k] = nums[i];
+            lastInsertNumber = nums[i];
+            k++;
+            inserted = 1;
+        } while (lastInsertNumber != nums[i] && inserted == 0);
+    }
+    return k;*/
+ 
