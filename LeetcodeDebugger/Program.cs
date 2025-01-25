a8e4d9ff8026afef47b13f796456695deb2f6471
Myo Set Paing <138693417+KraftReport@users.noreply.github.com>
2025-01-25 22:50:33 +0630

    leetcode

--- LeetcodeDebugger/Program.cs
+++ LeetcodeDebugger/Program.cs
@@ -1,4 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Linq.Expressions;
+
 Console.WriteLine("Hello, World!");
 
 /*int[] nums = { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4 };
@@ -17,4 +19,32 @@ Console.WriteLine("Hello, World!");
         } while (lastInsertNumber != nums[i] && inserted == 0);
     }
     return k;*/
- 
+
+/*string romanNumber = "IIII";
+
+int result = 0;
+
+Dictionary<char, int> map = new Dictionary<char, int>()
+{
+    {'I',1 },
+    {'V',5 },
+    {'X',10 },
+    {'L',50 },
+    {'C',100 },
+    {'D',500 },
+    {'M',1000 }
+};
+
+for(int i = 0; i< romanNumber.Length; i++)
+{
+    int currentValue = map[romanNumber[i]];
+
+    if(i+1 < romanNumber.Length && map[romanNumber[i+1]] > currentValue)
+    {
+        result -= currentValue;
+    }
+    else
+    {
+        result += currentValue;
+    }
+}*/
